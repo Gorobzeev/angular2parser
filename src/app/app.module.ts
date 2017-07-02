05b7de8606082f475be53e84e6e95ada3119f56b
Stas Gorobzeev <staszp@gmail.com>
2017-07-02 13:30:59 +0300

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LibraryComponent } from './library/library.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LibraryComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule
