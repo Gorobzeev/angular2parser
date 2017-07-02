66be494c24884e5f2c5685496b753dc7d38277be
Stas Gorobzeev <staszp@gmail.com>
2017-07-02 18:48:32 +0300

    start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router'
 
-import { AppComponent } from './app.component';
-import { LibraryComponent } from './library/library.component';
-import { AboutComponent } from './about/about.component';
+import {AppComponent} from './app.component';
+import {LibraryComponent} from './library/library.component';
+import {AboutComponent} from './about/about.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LibraryComponent,
-    AboutComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        LibraryComponent,
+        AboutComponent
+    ],
+    imports: [
+        BrowserModule,
+        RouterModule.forRoot([
+            {path: 'library', component: LibraryComponent},
+            {path: 'about', component: AboutComponent}
+        ])
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {
+}
