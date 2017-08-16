2275931681d862348ea38d51322b52f974beaab5
Stas Gorobzeev <staszp@gmail.com>
2017-08-16 17:06:01 +0300

    add view simple login page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  showMenu = true;
+
+  constructor(private router: Router) {
+  }
+
+  ngOnInit() {
+    // listenging to routing navigation event
+    this.router.events.subscribe(event => this.modifyMenu(event));
+  }
+
+  modifyMenu(location) {
+    // alert(location.url)
+    this.showMenu = location.url !== '/login';
+  }
 }
